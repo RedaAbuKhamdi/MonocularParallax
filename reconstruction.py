8c3e422c59ce12a7dddf6cd6c261f1181bb7f6b5
Reda Abu-Khamdi <rabouhamdi@gmail.com>
2025-02-26 14:06:19 +0200

    Try ellipse

--- reconstruction.py
+++ reconstruction.py
@@ -17,6 +17,55 @@ def atoi(text):
 def natural_keys(text):
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
+SEARCH_LENGTH = 100
+EPSILON = 1e-3
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+@jit(fastmath = True)
+def calculate_angle(point1, point2, center):
+    vector1 = point1 - center
+    vector2 = point2 - center
+    return np.arccos(np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
+@jit(fastmath = True)
+def calculate_angle_wurf(p1 : np.ndarray, p2 : np.ndarray, p3 : np.ndarray, p4 : np.ndarray, center : np.ndarray):
+    sin_alpha = np.sin(calculate_angle(p1, p2, center))
+    sin_beta = np.sin(calculate_angle(p2, p3, center))
+    sin_gamma = np.sin(calculate_angle(p3, p4, center))
+    return 0 if ((sin_alpha + sin_beta + sin_gamma) * sin_beta) == 0 else float(sin_alpha * sin_gamma / ((sin_alpha + sin_beta + sin_gamma) * sin_beta))
+@jit(nopython = True, parallel = True)
+def search_support(i : int, j : int, k : int, frames : np.ndarray, center : np.ndarray, indicies : np.ndarray):
+    vals = np.zeros(len(frames[3]))
+    for l in prange(indicies.shape[0]):
+        vals[indicies[l]] = calculate_angle_wurf(frames[0][i], frames[1][j], frames[2][k], frames[3][indicies[l]], center)
+    return vals
+def find_points_for_given_point(center : np.ndarray, frames : np.ndarray, i : int):
+        best_points = np.zeros((4, 2))
+        best_val = np.inf
+        indicies = ModuleNotFoundError
+        for j in np.nonzero(
+                np.sum(((frames[1] - frames[0][i])**2), axis=1) < (SEARCH_LENGTH * SEARCH_LENGTH)
+                )[0]:
+            for k in np.nonzero(
+                np.sum(((frames[2] - frames[1][j])**2), axis=1) < (SEARCH_LENGTH * SEARCH_LENGTH)
+                )[0]:
+                vals = search_support(i, j, k, frames, center, np.nonzero(
+                np.sum(((frames[3] - frames[2][k])**2), axis=1) < (SEARCH_LENGTH * SEARCH_LENGTH)
+                )[0])
+                differences = np.abs(vals - 1/3)
+                min_val = np.argmin(differences)
+                min_wurf = vals[min_val]
+                if differences[min_val] < min(EPSILON, best_val):
+                    best_points[0] = frames[0][i]
+                    best_points[1] = frames[1][j]
+                    best_points[2] = frames[2][k]
+                    best_points[3] = frames[3][min_val]
+                    indicies = (i, j, k, min_val)
+                    best_val = differences[min_val]
+        return best_points, indicies
 class InverseCylinder:
     def __init__(self, inverse : bool = True):
         self.inverse = inverse
@@ -25,7 +74,7 @@ class InverseCylinder:
             if isfile(join("./inputFrames", image)):
                 print("./inputFrames/" + image)
                 slice = np.transpose(io.imread("./inputFrames/" + image))
-                slice = ndimage.binary_erosion(slice.copy(), ndimage.generate_binary_structure(2, 3))
+                slice = ndimage.binary_erosion(slice.copy() > 0, ndimage.generate_binary_structure(2, 3))
                 self.images.append(slice)
         self.images = np.array(self.images)
         print(self.images.shape)
@@ -62,14 +111,11 @@ class InverseCylinder:
         
         return max_x - min_x
 
-    def fit_ellipse(self):
-        
-
     def calculate_ellipse_equation(seld, track : np.ndarray):
         A = np.zeros((5, 5))
         B = np.ones((5))
         for i in range(5):
-            pt = track[i]
+            pt = track[i+1]
             A[i, 0] = pt[0]**2
             A[i, 1] = pt[1]**2
             A[i, 2] = pt[0]*pt[1]
@@ -131,7 +177,67 @@ class InverseCylinder:
         sin_beta = np.sin(self.calculate_angle(self.chosen_points[start - 1][index], self.chosen_points[start - 2][index], center))
         sin_gamma = np.sin(self.calculate_angle(self.chosen_points[start - 2][index], self.chosen_points[start - 3][index], center))
         return float(sin_alpha * sin_gamma / ((sin_alpha + sin_beta + sin_gamma) * sin_beta))
-    
+    def track_point(self, center : np.ndarray, index : int = 0):
+        if self.points is None:
+            raise Exception("No extracted points! Run extract_points() first.")
+        if os.path.isfile("./trajectories/trajectory{}.npy".format(index)):
+            return np.load("./trajectories/trajectory{}.npy".format(index))
+        trajectory = np.zeros((self.images.shape[0], 2), dtype=np.float32)
+        first_points, indicies = find_points_for_given_point(center, self.points, index)
+        indicies = list(indicies)
+        if first_points is None:
+            raise Exception("No points found!")
+
+        trajectory[0] = first_points[0]
+        trajectory[1] = first_points[1]
+        trajectory[2] = first_points[2]
+        trajectory[3] = first_points[3]
+        
+        for i in range(4, self.images.shape[0]):
+            found_point = False
+            min_val = np.inf
+            indicies.append(None)
+            for j in np.nonzero(
+                    np.sum(((self.points[i] - trajectory[i-1])**2), axis=1) < (SEARCH_LENGTH * SEARCH_LENGTH)
+                )[0]:
+                wurf = calculate_angle_wurf(trajectory[i-3], trajectory[i-2], trajectory[i-1], self.points[i][j], center)
+                if np.abs(wurf - 1/3) < min(EPSILON, min_val):
+                    trajectory[i] = self.points[i][j]
+                    found_point = True
+                    min_val = np.abs(wurf - 1/3)
+                    indicies[i] = j
+            if not found_point:
+                raise Exception("No point was found for trajectory!")
+            
+        np.save("./trajectories/trajectory{}.npy".format(index), trajectory)
+        for i, index in enumerate(indicies):
+            self.points[i][index][0] = 0
+            self.points[i][index][1] = 0
+        return trajectory
+
+    def calculate_contrasting_points(self):
+        chosen_points = []
+
+        if self.points is None:
+            raise Exception("No extracted points! Run extract_points() first.")
+        x = self.images[0].shape[0] // 2 
+        y = self.images[0].shape[1] // 2 
+        print("x = {}, y = {}".format(x, y))
+
+        for i in tqdm(range(self.points[0].shape[0])):
+            if self.points[0][i][0] == 0 and self.points[0][i][1] == 0:
+                #print("already have {}".format(i))
+                continue
+            try:
+                chosen_points.append(
+                        self.track_point(np.array([x, y]), i)
+                    )
+                
+                print("Found track for {}".format(i))
+            except Exception as e:  
+                #print(traceback.format_exc())
+                pass
+        return chosen_points
     def visualize(self, chosen_points = []):
         fig, ax = plt.subplots()
         print(chosen_points)
@@ -172,10 +278,11 @@ class InverseCylinder:
             cylinder_pts = self.points[n]
             ax.scatter(track[:,0], track[:, 1], color = "red")
             ax.scatter(cylinder_pts[:,0], cylinder_pts[:, 1], color = "blue")   
-            preds = model.predict_xy(np.linspace(0, 2 * np.pi, 100)) 
-            if (len(ellipse_pts) > 0):
-                ax.scatter(ellipse_pts[:,0], ellipse_pts[:, 1], color = "green")   
-            ax.scatter(preds[:,0], preds[:, 1], color = "purple")  
+            # if (len(ellipse_pts) > 0):
+            #     ax.scatter(ellipse_pts[:,0], ellipse_pts[:, 1], color = "green")   
+            # if model is not None:  
+            #     preds = model.predict_xy(np.linspace(0, 2 * np.pi, 100)) 
+            #     ax.scatter(preds[:,0], preds[:, 1], color = "purple")  
 
 
         update(0)
@@ -190,8 +297,7 @@ class InverseCylinder:
                 result, valid, model = self.calculate_ellipse(track.copy())
             except: 
                 print(traceback.format_exc())
-            if valid:
-                self.visualize_track(track, result, model)
+            self.visualize_track(track, result, model)
 
     def track_by_proximity(self):
         indecies = np.argsort(self.points[0][:,1])
